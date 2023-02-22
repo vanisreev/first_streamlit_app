3dd6a5d9d84e928190dcca5998bc888ac118293b
vanisreev <vanisreev@gmail.com>
2023-02-22 12:55:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ streamlit.header("build your own smpothie 🍹")
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-streamlit.multiselect("pick soe fruits",list(my_fruit_list.index))
+selected_fruit_list = streamlit.multiselect("pick some fruits",list(my_fruit_list.index)['Avocado','Grapes'])
 streamlit.dataframe(my_fruit_list)
-streamlit.multiselect("pick soe fruits",list(my_fruit_list.index),['Avocado','Grapes'])
+fruits_to_show = my_fruit_list.loc[selected_fruit]
+streamlit.dataframe(fruits_to_show)
